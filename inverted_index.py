65c36086e30f33b6c21e820778a69022ebb41573
parsaenami <parsaenami@outlook.com>
2020-06-10 14:58:06 +0430

    added documentation for InvertedIndex class

--- inverted_index.py
+++ inverted_index.py
@@ -1,22 +1,48 @@
+from typing import List
+
 class InvertedIndex:
+    """
+    This class represents an inverted index object.
+    """
+
     def __init__(self):
         self.posting_lists = {}
         
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.posting_lists)
     
-    def has_term(self, term):
+    def has_term(self, term: str) -> bool:
+        """
+        Tells if a word is present in the entire indexing list or not.
+        """
+
         return term in list(self.posting_lists.keys())
 
-    def add(self, term, doc_id, position):
+    def add(self, term: str, doc_id: int, position: int) -> None:
+        """
+        Adds a term to indexing list. It also saves the positions, so we can have a positional index.
+        """
+
         self.posting_lists.setdefault(term, {}).setdefault(
             doc_id, []).append(position)
 
-    def get_docs(self, term):
+    def get_docs(self, term: str) -> List[int]:
+        """
+        Returns a list consisting of ids of document that contain a particular word.
+        """
+
         return list(self.posting_lists[term].keys())
 
-    def doc_frequency(self, term):
-        return len(self.postings_lists[term])
+    def doc_frequency(self, term: str) -> int:
+        """
+        Returns the number of documents that contain a particular word.
+        """
 
-    def term_frequency(self, term, doc_id):
-        return len(self.postings_lists[term][doc_id])
+        return len(self.posting_lists[term])
+
+    def term_frequency(self, term: str, doc_id: int) -> int:
+        """
+        Returns the number of a word occurrences in a document.
+        """
+
+        return len(self.posting_lists[term][doc_id])
